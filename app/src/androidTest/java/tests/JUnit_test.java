8829542c25f206bf7ecb04ef8169e6edabf9f7b7
kmklein <froggy123>
2016-04-13 10:58:20 -0700

    Junit Test

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,34 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.kmklein.lab3.MainActivity;
+import com.example.kmklein.lab3.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by kmklein on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+
+    public JUnit_test() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = getActivity();
+    }
+
+    public void test_first() {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+        assertEquals("Hello World!", tester);
+    }
+}
